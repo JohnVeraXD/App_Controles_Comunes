f24bcf2f64a3652ebb045dc2cff088adeea2ef96
John Vera <johnveramacias2002@gmail.com>
2023-06-22 22:38:27 -0500

    Controles puestos

--- app/src/main/java/com/example/app_controles_comunes/MainActivity.java
+++ app/src/main/java/com/example/app_controles_comunes/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.app_controles_comunes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBiding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private boolean validarCorreo(){
+        //Sacar la referencia
+        EditText txtcorreo = (EditText)findViewById(R.id.txtCorreo);
+        String correo = txtcorreo.getText().toString();
+        return if(correo.isEmpty()){
+            binding.correo
+        }
     }
 }
